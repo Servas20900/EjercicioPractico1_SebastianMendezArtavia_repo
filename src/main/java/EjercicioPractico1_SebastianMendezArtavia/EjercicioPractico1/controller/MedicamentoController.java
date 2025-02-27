01ced4830c5955cfa3cdadda4d768d09aefe728e
Sebastián Roberto Méndez Artavia <sebasmendeza09@gmail.com>
2025-02-26 21:47:55 -0600

    update
    listados

--- src/main/java/EjercicioPractico1_SebastianMendezArtavia/EjercicioPractico1/controller/MedicamentoController.java
+++ src/main/java/EjercicioPractico1_SebastianMendezArtavia/EjercicioPractico1/controller/MedicamentoController.java
@@ -1,5 +1,25 @@
 package EjercicioPractico1_SebastianMendezArtavia.EjercicioPractico1.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import EjercicioPractico1_SebastianMendezArtavia.EjercicioPractico1.service.MedicamentoService;
+
+@Controller
+@RequestMapping("/medicamento")
 public class MedicamentoController {
     
+    @Autowired
+    private MedicamentoService medicamentoService;
+
+    @GetMapping("/listado")
+    public String listarMedicamentos(Model model) {
+        var medicamentos = medicamentoService.getMedicamentos();
+        model.addAttribute("medicamentos", medicamentos);
+        model.addAttribute("totalMedicamentos", medicamentos.size());
+        return "/medicamento/listado";
+    }
 }
